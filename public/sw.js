edda018e66f9897fbba5289aa3e05e0c1cb1ad0d
vishswati1011 <sati.v1011@gmail.com>
2023-01-12 15:50:37 +0530

    service worker file added

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+let cacheData="blogV1";
+
+this.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                "/static/js/bundle.js",
+                "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+                "/static/js/main.chunk.js",
+                "/static/js/0.chunk.js",
+                "/index.html",
+                "/",
+                "/favicon.ico",                
+                "/home",
+                "/employee",
+                "/project",
+                "/blog",
+                "/addBlog",
+                "/profile"
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    // console.log("navigator",navigator.onLine)
+    // if(!navigator.onLine){
+        event.respondWith(
+            caches.match(event.request).then((result)=>{
+                if(result){
+                    return result
+                }
+                let requestUrl=event.request.clone();
+                return fetch(requestUrl);
+            })
+        )
+    // }
+})
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ProSidebarProvider } from 'react-pro-sidebar';
-
+import swDev from './swDev';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -18,3 +18,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+swDev(); 
